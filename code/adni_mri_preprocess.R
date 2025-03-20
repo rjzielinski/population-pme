afc08fe66dfa8ab0d8eb3ea1ad5e0d721be465c5
Robert Zielinski <rjz1996@outlook.com>
2025-03-20 10:46:26 -0400

    fix ouptut path

--- code/adni_mri_preprocess.R
+++ code/adni_mri_preprocess.R
@@ -9,7 +9,7 @@ library(tidyverse)
 library(lubridate)
 # library(ANTsRCore)
 
-sub_dirs <- list.dirs("data/adni", recursive = FALSE)
+sub_dirs <- list.dirs("data/ADNI", recursive = FALSE)
 
 nii_scans <- list()
 
@@ -27,8 +27,7 @@ ncores <- parallel::detectCores() - 1
 cl <- parallel::makeCluster(ncores, type = "FORK")
 doParallel::registerDoParallel(cl = cl)
 
-# foreach (dir_idx = 1:length(sub_dirs)) %dopar% {
-foreach(dir_idx = 1:8) %dopar% {
+foreach (dir_idx = 1:length(sub_dirs)) %dopar% {
   img_dirs <- list.files(sub_dirs[dir_idx], recursive = TRUE, full.names = TRUE) %>%
     gsub(pattern = "([^/]+$)", replacement = "") %>%
     unique()
@@ -36,7 +35,7 @@ foreach(dir_idx = 1:8) %dopar% {
   nii_scans[[dir_idx]] <- list()
 
   for (img_idx in 1:length(img_dirs)) {
-    proc_dir <- paste0("data/adni_processed_fsl", gsub(pattern = "data/adni", replacement = "", img_dirs[img_idx]))
+    proc_dir <- paste0("data/adni_processed_fsl", gsub(pattern = "data/ADNI", replacement = "", img_dirs[img_idx]))
     if (file.exists(paste0(proc_dir, "/_all_fast_origsegs.nii.gz"))) {
       next
     } else {
