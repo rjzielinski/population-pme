4bf55dbaed341e770b9c73dfdff7be3b2fd9a48f
Robert Zielinski <rjz1996@outlook.com>
2025-04-11 11:58:44 -0400

    parallelize cross-validation

--- code/functions/fit_weighted_spline.R
+++ code/functions/fit_weighted_spline.R
@@ -1,4 +1,5 @@
-fit_weighted_spline <- function(x, params, weights, smoothing_vals, folds) {
+fit_weighted_spline <- function(x, params, weights, smoothing_vals, folds, parallel = "sequential") {
+  require(future, quietly = TRUE)
   require(pme, quietly = TRUE)
   require(purrr, quietly = TRUE)
   k <- length(unique(folds))
@@ -7,43 +8,58 @@ fit_weighted_spline <- function(x, params, weights, smoothing_vals, folds) {
   smoothing_error <- vector()
   lambda <- smoothing_vals
 
+  cores <- availableCores()
+
   for (smoothing_idx in seq_along(smoothing_vals)) {
     coefs[[smoothing_idx]] <- list()
-    fold_errors <- vector()
-    for (k_idx in 1:k) {
-      train_params <- params[!(folds == k_idx), ] %>%
-        matrix(ncol = d)
-      fold_params <- params[folds == k_idx, ] %>%
-        matrix(ncol = d)
-      train_param_mat <- cbind(1, train_params)
-      train_x <- x[!(folds == k_idx), ]
-      fold_x <- x[folds == k_idx, ]
-      train_weights <- diag(weights[!(folds == k_idx)])
-      train_E <- calcE(train_params, 4 - ncol(train_params))
-      coefs[[smoothing_idx]][[k_idx]] <- solve_weighted_spline(
-        train_E,
-        train_weights,
-        train_param_mat,
-        train_x,
-        lambda[smoothing_idx],
-        ncol(train_params),
-        ncol(train_x)
-      )
-      fold_embedding <- function(parameters) {
-        as.vector(
-          (t(coefs[[smoothing_idx]][[k_idx]][seq_len(nrow(train_x)), ]) %*% etaFunc(parameters, train_params, 4 - d)) +
-            (t(coefs[[smoothing_idx]][[k_idx]][(nrow(train_x) + 1):(nrow(train_x) + d + 1), ]) %*% matrix(c(1, parameters), ncol = 1))
-        )
-      }
-      fold_errors[k_idx] <- map(
-        seq_len(nrow(fold_x)),
-        ~ weights[folds == k_idx][.x] * dist_euclidean(
-          fold_x[.x, ], fold_embedding(fold_params[.x, ])
-        )^2
-      ) %>%
-        reduce(c) %>%
-        mean()
+
+    if (parallel == "sequential") {
+      plan(sequential)
+    } else if (parallel == "multisession") {
+      plan(multisession, workers = min(cores, k))
+    } else if (parallel == "multicore") {
+      plan(multicore, workers = min(cores, k))
     }
+    fold_output <- list()
+    for (k_idx in seq_len(k)) {
+      fold_output[[k_idx]] <- future({
+        train_params <- params[!(folds == k_idx), ] %>%
+          matrix(ncol = d)
+        fold_params <- params[folds == k_idx, ] %>%
+          matrix(ncol = d)
+        train_param_mat <- cbind(1, train_params)
+        train_x <- x[!(folds == k_idx), ]
+        fold_x <- x[folds == k_idx, ]
+        train_weights <- diag(weights[!(folds == k_idx)])
+        train_E <- calcE(train_params, 4 - ncol(train_params))
+        coefs[[smoothing_idx]][[k_idx]] <- solve_weighted_spline(
+          train_E,
+          train_weights,
+          train_param_mat,
+          train_x,
+          lambda[smoothing_idx],
+          ncol(train_params),
+          ncol(train_x)
+        )
+        fold_embedding <- function(parameters) {
+          as.vector(
+            (t(coefs[[smoothing_idx]][[k_idx]][seq_len(nrow(train_x)), ]) %*% etaFunc(parameters, train_params, 4 - d)) +
+              (t(coefs[[smoothing_idx]][[k_idx]][(nrow(train_x) + 1):(nrow(train_x) + d + 1), ]) %*% matrix(c(1, parameters), ncol = 1))
+          )
+        }
+        error_val <- map(
+          seq_len(nrow(fold_x)),
+          ~ weights[folds == k_idx][.x] * dist_euclidean(
+            fold_x[.x, ], fold_embedding(fold_params[.x, ])
+          )^2
+        ) %>%
+          reduce(c) %>%
+          mean()
+        error_val
+      }, seed = TRUE)
+    }
+    fold_errors <- map(fold_output, ~ value(.x)) %>%
+      reduce(c)
     smoothing_error[smoothing_idx] <- mean(fold_errors)
     if (length(smoothing_error) >= 5) {
       if (!is.unsorted(smoothing_error[(length(smoothing_error) - 4):length(smoothing_error)])) {
