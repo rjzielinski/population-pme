7ca1d1bd012799c4d0b69841fee190d6258218cb
Robert Zielinski <rjz1996@outlook.com>
2026-03-11 12:34:39 -0400

    add bootstrap test

--- code/functions/adni_modeling/functional_anova_pointwise.R
+++ code/functions/adni_modeling/functional_anova_pointwise.R
@@ -6,43 +6,148 @@ functional_anova_pointwise <- function(
   id_groups,
   n_params,
   test_type = "f_type",
-  alpha = 0.05
+  alpha = 0.05,
+  bootstrap = FALSE,
+  n_bootstrap = 1000
 ) {
-  require(furrr)
-  require(purrr)
-
-  param_bounds <- list()
-  param_grids <- list()
-  time_points_list <- list()
+  require(doFuture, quietly = TRUE)
+  require(foreach, quietly = TRUE)
+  require(furrr, quietly = TRUE)
+  require(purrr, quietly = TRUE)
 
   n_partitions <- length(additive_model)
   n_groups <- length(additive_model[[1]]$group_embeddings)
   n_individuals <- length(additive_model[[1]]$id_embeddings)
 
-  group_n <- map(groups, ~ sum(id_groups == .x)) |>
-    reduce(c)
+  param_grids <- calc_param_grids(params, n_params, interval = 0.25)
 
-  population_embeddings <- list()
-  group_embeddings <- list()
-  id_embeddings <- list()
+  print("Computing Embeddings")
 
-  for (param_idx in seq_len(length(params))) {
+  embeddings <- calc_embeddings(
+    additive_model,
+    param_grids,
+    n_groups,
+    n_individuals,
+    id_groups
+  )
+
+  sum_squares <- calc_sum_squares(
+    embeddings,
+    param_grids,
+    groups,
+    ids
+  )
+
+  test_stat <- list()
+  rejected <- list()
+
+  for (partition_idx in seq_len(n_partitions)) {
+    test_stat[[partition_idx]] <- (sum_squares$ssh[[partition_idx]] /
+      (n_groups - 1)) /
+      (sum_squares$sse[[partition_idx]] / (n_individuals - n_groups))
+  }
+
+  if (bootstrap == FALSE) {
+    if (test_type == "f_type") {
+      critical_value <- qf(1 - alpha, n_groups - 1, n_individuals - n_groups)
+    } else if (test_type == "chisq_type") {
+      critical_value <- qchisq(1 - alpha, n_groups - 1) / (n_groups - 1)
+    }
+    for (partition_idx in seq_along(additive_model)) {
+      rejected[[partition_idx]] <- test_stat[[partition_idx]] > critical_value
+    }
+  } else {
+    bootstrap_test_stats <- test_stat
+
+    bootstrap_test_stats <- map(
+      bootstrap_test_stats,
+      ~ matrix(NA, nrow = n_bootstrap, ncol = length(.x))
+    )
+
+    print("Getting Bootstrap Sample...")
+
+    p <- progressor(n_bootstrap)
+
+    bootstrap_results <- foreach(
+      bootstrap_idx = seq_len(n_bootstrap),
+      .options.future = list(seed = TRUE)
+    ) %dofuture%
+      {
+        bootstrap_embeddings <- calc_bootstrap_embeddings(
+          embeddings,
+          groups,
+          ids,
+          id_groups
+        )
+
+        bootstrap_sum_squares <- calc_sum_squares(
+          bootstrap_embeddings,
+          param_grids,
+          groups,
+          ids
+        )
+
+        test_stats <- map(
+          seq_len(n_partitions),
+          ~ (bootstrap_sum_squares$ssh[[.x]] /
+            (n_groups - 1)) /
+            (bootstrap_sum_squares$sse[[.x]] /
+              (n_individuals - n_groups))
+        )
+
+        p()
+
+        test_stats
+      }
+
+    for (bootstrap_idx in seq_len(n_bootstrap)) {
+      for (partition_idx in seq_len(n_partitions)) {
+        bootstrap_test_stats[[partition_idx]][
+          bootstrap_idx,
+        ] <- bootstrap_results[[bootstrap_idx]][[partition_idx]]
+      }
+    }
+
+    # calculate critical values at each parameter point
+    critical_value <- map(
+      bootstrap_test_stats,
+      ~ apply(.x, 2, quantile, 1 - alpha, na.rm = TRUE)
+    )
+
+    for (partition_idx in seq_len(n_partitions)) {
+      rejected[[partition_idx]] <- test_stat[[partition_idx]] >
+        critical_value[[partition_idx]]
+    }
+  }
+
+  test_out <- list(
+    params = param_grids,
+    embeddings = embeddings,
+    rejected = rejected
+  )
+}
+
+calc_param_grids <- function(params, n_params, interval = 0.25) {
+  require(Rfast, quietly = TRUE)
+
+  param_bounds <- list()
+  param_grids <- list()
+  time_points_list <- list()
+
+  for (param_idx in seq_along(params)) {
     time_points_list[[param_idx]] <- unique(params[[param_idx]][, 1])
     times <- seq(
       from = 0,
       to = max(time_points_list[[param_idx]]),
-      by = 0.25
+      by = interval
     )
 
-    param_bounds[[param_idx]] <- colMinsMaxs(lhipp_test_out$params[[
-      param_idx
-    ]][,
-      -1
-    ])
+    param_bounds[[param_idx]] <- colMinsMaxs(params[[param_idx]][, -1])
     d <- ncol(param_bounds[[param_idx]])
 
     param_list <- list()
     param_list[[1]] <- times
+
     for (dim in seq_len(d)) {
       param_range <- abs(
         param_bounds[[param_idx]][2, dim] - param_bounds[[param_idx]][1, dim]
@@ -57,73 +162,113 @@ functional_anova_pointwise <- function(
     param_grids[[param_idx]] <- expand.grid(param_list)
   }
 
+  param_grids
+}
+
+calc_embeddings <- function(
+  additive_model,
+  param_grids,
+  n_groups,
+  n_individuals,
+  id_groups
+) {
+  require(foreach, quietly = TRUE)
+  require(furrr, quietly = TRUE)
+  require(progressr, quietly = TRUE)
+
+  population_embeddings <- list()
+  group_embeddings <- list()
+  id_embeddings <- list()
+
   for (partition_idx in seq_along(additive_model)) {
+    p <- progressor(nrow(param_grids[[partition_idx]]))
     population_embeddings[[partition_idx]] <- future_map(
       seq_len(nrow(param_grids[[partition_idx]])),
-      ~ additive_model[[
-        partition_idx
-      ]]$population_embedding$embedding_map(unlist(param_grids[[partition_idx]][
-        .x,
-      ])),
+      ~ {
+        p()
+        additive_model[[
+          partition_idx
+        ]]$population_embedding$embedding_map(unlist(param_grids[[
+          partition_idx
+        ]][
+          .x,
+        ]))
+      },
       .options = furrr_options(seed = TRUE)
     ) |>
       reduce(rbind)
 
-    # with_progress({
-    #   p <- progressor(n_groups)
     part_group_embeddings <- foreach(
       group_idx = seq_len(n_groups),
       .options.future = list(seed = TRUE)
-    ) %dofuture%
+    ) %do%
       {
-        map(
+        p <- progressor(nrow(param_grids[[partition_idx]]))
+        future_map(
           seq_len(nrow(param_grids[[partition_idx]])),
-          ~ additive_model[[partition_idx]]$group_embeddings[[
-            group_idx
-          ]]$embedding_map(unlist(param_grids[[partition_idx]][.x, ]))
+          ~ {
+            p()
+            additive_model[[partition_idx]]$group_embeddings[[
+              group_idx
+            ]]$embedding_map(unlist(param_grids[[partition_idx]][.x, ]))
+          },
+          .options = furrr_options(seed = TRUE)
         ) |>
           reduce(rbind)
       }
-    # })
 
-    # with_progress({
-    #   p <- progressor(n_individuals)
-    part_id_embeddings <- foreach(
-      id_idx = seq_len(n_individuals),
-      .options.future = list(seed = TRUE)
-    ) %dofuture%
+    p <- progressor(n_individuals)
+    part_id_embeddings <- foreach(id_idx = seq_len(n_individuals)) %do%
       {
-        map(
+        p()
+        future_map(
           seq_len(nrow(param_grids[[partition_idx]])),
           ~ additive_model[[partition_idx]]$id_embeddings[[
             id_idx
-          ]]$embedding_map(unlist(param_grids[[partition_idx]][.x, ]))
+          ]]$embedding_map(unlist(param_grids[[partition_idx]][.x, ])),
+          .options = furrr_options(seed = TRUE)
         ) |>
           reduce(rbind)
-        #      p()
       }
-    # })
 
     group_embeddings[[partition_idx]] <- part_group_embeddings
     id_embeddings[[partition_idx]] <- part_id_embeddings
   }
 
+  embedding_list <- list(
+    population_embeddings = population_embeddings,
+    group_embeddings = group_embeddings,
+    id_embeddings = id_embeddings,
+    group_assignments = id_groups
+  )
+}
+
+calc_sum_squares <- function(
+  embeddings,
+  param_grids,
+  groups,
+  ids
+) {
+  require(foreach, quietly = TRUE)
+
+  population_embeddings <- embeddings$population_embeddings
+  group_embeddings <- embeddings$group_embeddings
+  id_embeddings <- embeddings$id_embeddings
+  id_groups <- embeddings$group_assignments
+
+  group_n <- map(groups, ~ sum(id_groups == .x)) |>
+    reduce(c)
+
   ssh <- list()
   sse <- list()
-  # sample_cov <- list()
-  f_score <- list()
-  chisq_score <- list()
 
-  rejected <- list()
-  p_vals <- list()
-
-  for (partition_idx in seq_along(additive_model)) {
+  for (partition_idx in seq_along(param_grids)) {
     ssh[[partition_idx]] <- foreach(
       param_idx = seq_len(nrow(param_grids[[partition_idx]]))
     ) %do%
       {
         map(
-          seq_along(groups),
+          seq_along(group_embeddings[[partition_idx]]),
           ~ group_n[.x] *
             sum(group_embeddings[[partition_idx]][[.x]][param_idx, -1]^2)
         ) |>
@@ -136,50 +281,116 @@ functional_anova_pointwise <- function(
     ) %do%
       {
         map(
-          seq_along(ids),
+          seq_along(id_embeddings[[partition_idx]]),
           ~ sum(id_embeddings[[partition_idx]][[.x]][param_idx, -1]^2)
         ) |>
           reduce(sum)
       } |>
       reduce(c)
-
-    # sample_cov[[partition_idx]] <- matrix(
-    #   nrow = nrow(param_grids[[partition_idx]]),
-    #   ncol = nrow(param_grids[[partition_idx]])
-    # )
-    #
-    # for (param_idx1 in nrow(param_grids[[partition_idx]])) {
-    #   for (param_idx2 in nrow(param_grids[[partition_idx]])) {
-    #     sample_cov_num <- map(
-    #       seq_along(ids),
-    #       ~ sum(
-    #         id_embeddings[[partition_idx]][[.x]][param_idx1, -1] *
-    #           id_embeddings[[partition_idx]][[.x]][param_idx2, -1]
-    #       )
-    #     ) |>
-    #       reduce(sum)
-    #
-    #     sample_cov[[partition_idx]][param_idx1, param_idx2] <- sample_cov_num /
-    #       (n_individuals - n_groups)
-    #   }
-    # }
-
-    f_score[[partition_idx]] <- (ssh[[partition_idx]] / (n_groups - 1)) /
-      (sse[[partition_idx]] / (n_individuals - n_groups))
   }
 
-  if (test_type == "f_type") {
-    critical_value <- qf(1 - alpha, n_groups - 1, n_individuals - n_groups)
-  } else if (test_type == "chisq_type") {
-    critical_value <- qchisq(1 - alpha, n_groups - 1) / (n_groups - 1)
-  }
-
-  for (partition_idx in seq_along(additive_model)) {
-    rejected[[partition_idx]] <- f_score[[partition_idx]] > critical_value
-  }
-
-  test_out <- list(
-    params = param_grids,
-    rejected = rejected
+  out <- list(
+    ssh = ssh,
+    sse = sse
   )
+}
+
+calc_bootstrap_embeddings <- function(embeddings, groups, ids, id_groups) {
+  population_embeddings <- embeddings$population_embeddings
+  group_embeddings <- embeddings$group_embeddings
+  id_embeddings <- embeddings$id_embeddings
+
+  bootstrap_id_idx <- sample(seq_along(ids), size = length(ids), replace = TRUE)
+  bootstrap_groups <- id_groups[bootstrap_id_idx] |>
+    sample(size = length(bootstrap_id_idx), replace = FALSE)
+
+  bootstrap_group_n <- map(groups, ~ sum(bootstrap_groups == .x)) |>
+    reduce(c)
+
+  bootstrap_embeddings <- list()
+  bootstrap_pop_embeddings <- list()
+  bootstrap_group_embeddings <- list()
+  bootstrap_id_embeddings <- list()
+
+  for (partition_idx in seq_along(population_embeddings)) {
+    partition_embeddings <- map(
+      bootstrap_id_idx,
+      ~ {
+        time_vals <- population_embeddings[[partition_idx]][, 1]
+        embedding_vals <- cbind(
+          time_vals,
+          population_embeddings[[partition_idx]][, -1] +
+            group_embeddings[[partition_idx]][[which(
+              groups == id_groups[.x]
+            )]][, -1] +
+            id_embeddings[[partition_idx]][[.x]][, -1]
+        )
+        embedding_vals
+      }
+    )
+
+    bootstrap_embeddings[[partition_idx]] <- partition_embeddings
+
+    bootstrap_pop_embeddings[[partition_idx]] <- reduce(
+      bootstrap_embeddings[[partition_idx]],
+      `+`
+    ) /
+      length(bootstrap_embeddings[[partition_idx]])
+
+    partition_group_embeddings <- foreach(group_idx = seq_along(groups)) %do%
+      {
+        group_indices <- which(bootstrap_groups == groups[group_idx])
+
+        group_mean <- (reduce(
+          bootstrap_embeddings[[partition_idx]][group_indices],
+          `+`
+        ) /
+          bootstrap_group_n[group_idx])
+
+        group_mean_adj <- group_mean - bootstrap_pop_embeddings[[partition_idx]]
+      }
+    bootstrap_group_embeddings[[partition_idx]] <- partition_group_embeddings
+
+    partition_id_embeddings <- foreach(
+      id_idx = seq_along(bootstrap_id_idx)
+    ) %do%
+      {
+        group_idx <- which(groups == bootstrap_groups[id_idx])
+        id_adj <- bootstrap_embeddings[[partition_idx]][[id_idx]] -
+          bootstrap_pop_embeddings[[partition_idx]] -
+          bootstrap_group_embeddings[[partition_idx]][[group_idx]]
+      }
+    bootstrap_id_embeddings[[partition_idx]] <- partition_id_embeddings
+  }
+
+  bootstrap_embeddings <- list(
+    population_embeddings = bootstrap_pop_embeddings,
+    group_embeddings = bootstrap_group_embeddings,
+    id_embeddings = bootstrap_id_embeddings,
+    group_assignments = bootstrap_groups
+  )
+}
+
+calc_sample_cov <- function() {
+  # sample_cov <- list()
+  # sample_cov[[partition_idx]] <- matrix(
+  #   nrow = nrow(param_grids[[partition_idx]]),
+  #   ncol = nrow(param_grids[[partition_idx]])
+  # )
+  #
+  # for (param_idx1 in nrow(param_grids[[partition_idx]])) {
+  #   for (param_idx2 in nrow(param_grids[[partition_idx]])) {
+  #     sample_cov_num <- map(
+  #       seq_along(ids),
+  #       ~ sum(
+  #         id_embeddings[[partition_idx]][[.x]][param_idx1, -1] *
+  #           id_embeddings[[partition_idx]][[.x]][param_idx2, -1]
+  #       )
+  #     ) |>
+  #       reduce(sum)
+  #
+  #     sample_cov[[partition_idx]][param_idx1, param_idx2] <- sample_cov_num /
+  #       (n_individuals - n_groups)
+  #   }
+  # }
 }
