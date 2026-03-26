d9a552f8ecbff5a83ab8d95a10f893cc8232d1ce
Robert Zielinski <rjz1996@outlook.com>
2026-03-26 15:50:30 -0400

    convert for cluster

--- code/adni_test.R
+++ code/adni_test.R
@@ -1,6 +1,7 @@
 library(doParallel)
 library(doFuture)
 library(foreach)
+library(future.mirai)
 library(here)
 library(listenv)
 library(plotly)
@@ -15,12 +16,15 @@ handlers("progress")
 options(future.globals.maxSize = 32 * 1024^3)
 options(renv.config.sandbox.enabled = FALSE)
 options(renv.config.auto.snapshot = FALSE)
-cores <- detectCores() - 2
+cores <- detectCores() - 1
+
+plot_progress <- FALSE
 
 source("code/functions/fit_weighted_spline.R")
 source("code/functions/print_SSD.R")
 source("code/functions/fit_adni_additive_model.R")
 source("code/functions/plot_id_pred.R")
+source("code/functions/plot_additive_model.R")
 source("code/functions/calculate_pme_reconstructions.R")
 source("code/functions/calculate_lpme_reconstructions.R")
 
@@ -29,8 +33,7 @@ map(
   source
 )
 
-# plan(multicore, workers = cores)
-plan(sequential)
+plan(cluster)
 
 ssd_ratio_threshold <- 5
 verbose <- TRUE
@@ -38,7 +41,7 @@ verbose <- TRUE
 epsilon <- 0.05
 max_iter <- 100
 
-read_data(n_individuals = 50)
+read_data(n_individuals = 250)
 
 gc()
 
@@ -75,6 +78,35 @@ init_params <- get_init_params(lhipp_init_lpme, lhipp_centers)
 partition_values <- unique(lhipp_partition)
 partition_values <- partition_values[order(partition_values)]
 
+if (plot_progress == TRUE) {
+  init_embeddings <- list()
+  for (partition_idx in seq_along(partition_values)) {
+    init_embeddings[[partition_idx]] <- future_map(
+      seq_len(nrow(init_params[[partition_idx]])),
+      ~ lhipp_init_lpme[[partition_idx]]$embedding_map(unlist(init_params[[
+        partition_idx
+      ]][.x, ])),
+      .options = furrr_options(seed = TRUE)
+    ) |>
+      reduce(rbind)
+  }
+  init_embeddings_full <- reduce(init_embeddings, rbind)
+
+  plot_ly(
+    x = init_embeddings_full[, 2],
+    y = init_embeddings_full[, 3],
+    z = init_embeddings_full[, 4],
+    frame = init_embeddings_full[, 1],
+    type = "scatter3d",
+    mode = "markers",
+    marker = list(size = 3)
+  )
+}
+
+group_vals <- unique(lhipp_surface_red$group)[order(unique(
+  lhipp_surface_red$group
+))]
+
 # Fit initial additive model estimate
 init_additive_mod <- list()
 for (partition_idx in seq_along(partition_values)) {
@@ -104,6 +136,15 @@ for (partition_idx in seq_along(partition_values)) {
     epsilon = 0.05,
     max_iter = 250,
     cores = cores
+  )
+}
+
+if (plot_progress == TRUE) {
+  plot_additive_model(
+    init_additive_mod,
+    init_params,
+    group_vals,
+    margin = 0.02
   )
 }
 
@@ -140,7 +181,7 @@ while (
     (ssd_ratio <= ssd_ratio_threshold) &
     (n <= (max_iter - 1))
 ) {
-  plan(multicore, workers = cores)
+  plan(cluster)
 
   ssd_prev <- ssd
   additive_model_old <- additive_model
@@ -178,6 +219,16 @@ while (
     )
   }
 
+  if (plot_progress == TRUE) {
+    fig <- plot_additive_model(
+      additive_model,
+      params,
+      group_vals,
+      margin = 0.02
+    )
+    print(fig)
+  }
+
   plan(sequential)
 
   print("Updating parameters")
@@ -206,9 +257,135 @@ while (
   }
 }
 
+if (plot_progress == TRUE) {
+  plot_additive_model(
+    additive_model,
+    params,
+    group_vals,
+    margin = 0.02
+  )
+}
+
+# opacity <- 0.75
+# line_width <- 2
+#
+# library(reticulate)
+# use_condaenv("lpme")
+# pv <- import("pyvista")
+#
+# time_points <- sort(unique(population_embeddings[, 1]))
+#
+# palette <- colorRampPalette(hcl.colors(8, palette = "viridis"))
+# palette_colors <- palette(151)
+# time_colors <- palette_colors[1:101][ceiling((time_points + 1e-10) * 10)]
+#
+# lhipp_plot <- pv$Plotter(shape = tuple(2L, 2L))
+#
+# for (time_idx in seq_along(time_points)) {
+#   if (time_points[time_idx] %% 1 == 0) {
+#     print(time_points[time_idx])
+#
+#     temp_pop <- population_embeddings[
+#       population_embeddings[, 1] == time_points[time_idx],
+#       -1
+#     ]
+#     temp_groups <- map(
+#       group_embeddings,
+#       ~ .x[.x[, 1] == time_points[time_idx], -1]
+#     )
+#
+#     pop_projection3 <- mesh_projection(temp_pop, axis = 3)
+#     group_projections <- map(temp_groups, ~ mesh_projection(.x, axis = 3))
+#
+#     lhipp_plot$subplot(0L, 0L)
+#     lhipp_plot$add_mesh(
+#       pop_projection3$boundary,
+#       show_edges = TRUE,
+#       color = time_colors[time_idx],
+#       line_width = line_width,
+#       opacity = opacity,
+#       label = paste0("Time = ", round(time_points[time_idx], 2))
+#     )
+#
+#     if (time_idx == 1) {
+#       lhipp_plot$add_text(
+#         "Population",
+#         position = "upper_left",
+#         font_size = 12
+#       )
+#     }
+#
+#     lhipp_plot$subplot(0L, 1L)
+#     lhipp_plot$add_mesh(
+#       group_projections[[1]]$boundary,
+#       show_edges = TRUE,
+#       color = time_colors[time_idx],
+#       line_width = line_width,
+#       opacity = opacity,
+#       label = paste0("Time = ", round(time_points[time_idx], 2))
+#     )
+#
+#     if (time_idx == 1) {
+#       lhipp_plot$add_text(
+#         group_vals[1],
+#         position = "upper_left",
+#         font_size = 12
+#       )
+#     }
+#
+#     lhipp_plot$subplot(1L, 0L)
+#     lhipp_plot$add_mesh(
+#       group_projections[[2]]$boundary,
+#       show_edges = TRUE,
+#       color = time_colors[time_idx],
+#       line_width = line_width,
+#       opacity = opacity,
+#       label = paste0("Time = ", round(time_points[time_idx], 2))
+#     )
+#
+#     if (time_idx == 1) {
+#       lhipp_plot$add_text(
+#         group_vals[2],
+#         position = "upper_left",
+#         font_size = 12
+#       )
+#     }
+#
+#     lhipp_plot$subplot(1L, 1L)
+#     lhipp_plot$add_mesh(
+#       group_projections[[3]]$boundary,
+#       show_edges = TRUE,
+#       color = time_colors[time_idx],
+#       line_width = line_width,
+#       opacity = opacity,
+#       label = paste0("Time = ", round(time_points[time_idx], 2))
+#     )
+#
+#     if (time_idx == 1) {
+#       lhipp_plot$add_text(
+#         group_vals[3],
+#         position = "upper_left",
+#         font_size = 12
+#       )
+#     }
+#
+#     lhipp_plot$link_views()
+#     lhipp_plot$camera_position <- "xy"
+#   } else {
+#     next
+#   }
+# }
+#
+# lhipp_plot$show()
+#
+# lhipp_plot$screenshot(
+#   "output/additive_mod_silhouette_plot.png"
+# )
+#
+
 # calculate final MSD and projections?
 
-plan(sequential)
+plan(cluster)
 
 print("Computing full projections")
 projection_list <- final_projections(
@@ -236,55 +413,95 @@ lhipp_test_out <- list(
 )
 saveRDS(lhipp_test_out, "output/lhipp_additive_model_250.RDS")
 
-lhipp_test_out <- readRDS("output/test_lhipp_additive_model.RDS")
-
-data <- lhipp_test_out$data
-
-groups <- lhipp_test_out$group_values
-ids <- lhipp_test_out$id_values
-id_groups <- map(
-  ids,
-  ~ filter(data, subid == .x) |>
-    pull(Group) |>
-    unique()
-) |>
-  reduce(c)
-
-plan(multicore, workers = cores)
-
-f_test_results <- functional_anova_pointwise(
-  lhipp_test_out$model,
-  lhipp_test_out$params,
-  groups,
-  ids,
-  id_groups,
-  n_params = 1000,
-  test_type = "f_type",
-  alpha = 0.05
-)
-
-chisq_test_results <- functional_anova_pointwise(
-  lhipp_test_out$model,
-  lhipp_test_out$params,
-  groups,
-  ids,
-  id_groups,
-  n_params = 1000,
-  test_type = "chisq_type",
-  alpha = 0.05
-)
-
-pointwise_bootstrap_results <- functional_anova_pointwise(
-  lhipp_test_out$model,
-  lhipp_test_out$params,
-  groups,
-  ids,
-  id_groups,
-  n_params = 1000,
-  test_type = "f_type",
-  alpha = 0.05,
-  bootstrap = TRUE,
-  n_bootstrap = 50
-)
-
-display_test_results(pointwise_bootstrap_results, groups)
+# lhipp_test_out <- readRDS("output/test_lhipp_additive_model.RDS")
+#
+# data <- lhipp_test_out$data
+#
+# groups <- lhipp_test_out$group_values
+# ids <- lhipp_test_out$id_values
+#
+# id_groups <- map(
+#   ids,
+#   ~ filter(data, subid == .x) |>
+#     pull(Group) |>
+#     unique()
+# ) |>
+#   reduce(c)
+#
+# plan(multicore, workers = cores)
+#
+# f_test_results <- functional_anova(
+#   lhipp_test_out$model,
+#   lhipp_test_out$params,
+#   groups,
+#   ids,
+#   id_groups,
+#   n_params = 1000,
+#   test_type = "f_type",
+#   alpha = 0.05
+# )
+#
+# f_test_bootstrap_results <- functional_anova(
+#   lhipp_test_out$model,
+#   lhipp_test_out$params,
+#   groups,
+#   ids,
+#   id_groups,
+#   n_params = 1000,
+#   test_type = "f_type",
+#   alpha = 0.05,
+#   bootstrap = TRUE,
+#   n_bootstrap = 1000
+# )
+#
+# chisq_test_results <- functional_anova(
+#   lhipp_test_out$model,
+#   lhipp_test_out$params,
+#   groups,
+#   ids,
+#   id_groups,
+#   n_params = 1000,
+#   test_type = "chisq_type",
+#   alpha = 0.05
+# )
+#
+# chisq_test_bootstrap_results <- functional_anova(
+#   lhipp_test_out$model,
+#   lhipp_test_out$params,
+#   groups,
+#   ids,
+#   id_groups,
+#   n_params = 1000,
+#   test_type = "chisq_type",
+#   alpha = 0.05,
+#   bootstrap = TRUE,
+#   n_bootstrap = 1000
+# )
+#
+# l2_norm_results <- functional_anova(
+#   lhipp_test_out$model,
+#   lhipp_test_out$params,
+#   groups,
+#   ids,
+#   id_groups,
+#   n_params = 1000,
+#   test_type = "l2_norm",
+#   alpha = 0.05
+# )
+#
+# l2_norm_bootstrap_results <- functional_anova(
+#   lhipp_test_out$model,
+#   lhipp_test_out$params,
+#   groups,
+#   ids,
+#   id_groups,
+#   n_params = 1000,
+#   test_type = "l2_norm",
+#   alpha = 0.05,
+#   bootstrap = TRUE,
+#   n_bootstrap = 1000
+# )
+#
+# display_test_results(pointwise_bootstrap_results, groups)
+#
+# display_test_results(f_test_results, groups)
