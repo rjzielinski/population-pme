604159c24978e0944e53f6a9a59350539f5e22d7
Robert Zielinski <rjz1996@outlook.com>
2026-04-06 15:21:42 -0400

    improve efficiency

--- code/functions/fit_weighted_spline.R
+++ code/functions/fit_weighted_spline.R
@@ -7,15 +7,19 @@ fit_weighted_spline <- function(x, params, weights, smoothing_vals) {
 
   coefs <- list()
   d <- ncol(params)
-  smoothing_error <- vector()
+  smoothing_error <- vector(mode = "numeric", length = length(smoothing_vals))
   lambda <- smoothing_vals
 
-  for (smoothing_idx in seq_along(smoothing_vals)) {
-    train_param_mat <- cbind(rep(1, nrow(params)), params)
-    train_weights <- diag(weights)
-    train_E <- calcE(params, 4 - d)
+  train_param_mat <- cbind(rep(1, nrow(params)), params)
+  train_weights <- weights
+  train_E <- calcE(params, 4 - d)
 
-    coefs[[smoothing_idx]] <- solve_weighted_spline_hat(
+  best_error <- 1e10
+  best_spline <- NULL
+  opt_run <- 1
+
+  for (smoothing_idx in seq_along(smoothing_vals)) {
+    spline_est <- solve_weighted_spline_hat(
       train_E,
       train_weights,
       train_param_mat,
@@ -25,16 +29,22 @@ fit_weighted_spline <- function(x, params, weights, smoothing_vals) {
       ncol(x)
     )
 
-    fitted_values <- coefs[[smoothing_idx]]$hat %*% x
+    fitted_values <- spline_est$hat %*% x
 
     RSS <- apply(x - fitted_values, 1, norm_euclidean)^2 |>
       sum()
 
     gcv_denom <- nrow(x) *
-      (1 - (sum(diag(coefs[[smoothing_idx]]$hat)) / nrow(x)))^2
+      (1 - (sum(diag(spline_est$hat)) / nrow(x)))^2
 
     smoothing_error[smoothing_idx] <- RSS / gcv_denom
-    #
+
+    if (smoothing_error[smoothing_idx] < best_error) {
+      best_error <- smoothing_error[smoothing_idx]
+      best_spline <- spline_est
+      opt_run <- smoothing_idx
+    }
+
     # if errors have been increasing for past 4 smoothing values, stop early
     if (smoothing_idx >= 4) {
       recent_errors <- smoothing_error[(smoothing_idx - 3):smoothing_idx]
@@ -44,22 +54,18 @@ fit_weighted_spline <- function(x, params, weights, smoothing_vals) {
     }
   }
 
-  opt_run <- which.min(smoothing_error)
-
-  opt_coefs <- coefs[[opt_run]]
-
   embedding_map <- function(parameters) {
     as.vector(
-      (t(opt_coefs$s) %*% etaFunc(parameters, params, 4 - d)) +
-        (t(opt_coefs$alpha) %*% matrix(c(1, parameters), ncol = 1))
+      (t(best_spline$s) %*% etaFunc(parameters, params, 4 - d)) +
+        (t(best_spline$alpha) %*% matrix(c(1, parameters), ncol = 1))
     )
   }
 
   return(
     list(
       embedding_map = embedding_map,
-      hat = opt_coefs$hat,
-      coefs = opt_coefs,
+      hat = best_spline$hat,
+      coefs = best_spline,
       lambda = smoothing_vals[opt_run]
     )
   )
