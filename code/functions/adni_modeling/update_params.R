df6150bf9268590ca9941e00cc03276113a3f514
Robert Zielinski <rjz1996@outlook.com>
2026-04-06 15:20:38 -0400

    optimize memory use

--- code/functions/adni_modeling/update_params.R
+++ code/functions/adni_modeling/update_params.R
@@ -20,36 +20,40 @@ update_params <- function(
     ids <- reduced_data |>
       filter(partition == partition_values[partition_idx]) |>
       pull(id)
+    id_indices <- match(ids, id_vals)
+
+    n_rows <- nrow(prev_params[[partition_idx]])
+
+    param_mat <- matrix(
+      0,
+      nrow = n_rows,
+      ncol = ncol(prev_params[[partition_idx]])
+    )
+    center_pred_mat <- matrix(
+      0,
+      nrow = n_rows,
+      ncol = ncol(centers[[partition_idx]])
+    )
+
+    partition_embeddings <- additive_model[[partition_idx]]$embeddings
 
     p <- progressor(nrow(centers[[partition_idx]]))
-    params[[partition_idx]] <- future_map(
-      seq_len(nrow(centers[[partition_idx]])),
-      ~ {
-        p()
-        projection_lpme(
-          centers[[partition_idx]][.x, ],
-          additive_model[[partition_idx]]$embeddings[[which(
-            id_vals == ids[.x]
-          )]],
-          prev_params[[partition_idx]][.x, ]
-        )
-      },
-      .options = furrr_options(seed = TRUE)
-    ) |>
-      reduce(rbind)
+    for (row_idx in seq_len(n_rows)) {
+      embedding_map <- partition_embeddings[[id_indices[row_idx]]]
 
-    p <- progressor(nrow(params[[partition_idx]]))
-    center_preds[[partition_idx]] <- future_map(
-      seq_len(nrow(params[[partition_idx]])),
-      ~ {
-        p()
-        additive_model[[partition_idx]]$embeddings[[which(
-          id_vals == ids[.x]
-        )]](params[[partition_idx]][.x, ])
-      },
-      .options = furrr_options(seed = TRUE)
-    ) |>
-      reduce(rbind)
+      param_est <- projection_lpme(
+        centers[[partition_idx]][row_idx, ],
+        embedding_map,
+        prev_params[[partition_idx]][row_idx, ]
+      )
+
+      param_mat[row_idx, ] <- param_est
+      center_pred_mat[row_idx, ] <- embedding_map(param_mat[row_idx, ])
+      p()
+    }
+
+    params[[partition_idx]] <- param_mat
+    center_preds[[partition_idx]] <- center_pred_mat
   }
 
   list(
