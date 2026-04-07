ba8f586954ddf279173002cab2322fcf1ffedf2c
Robert Zielinski <rjz1996@outlook.com>
2026-04-07 14:37:35 -0400

    simplify msd calculation

--- code/functions/adni_modeling/calc_msd.R
+++ code/functions/adni_modeling/calc_msd.R
@@ -3,17 +3,14 @@ calc_msd <- function(data, projections) {
   require(pme, quietly = TRUE)
   require(progressr, quietly = TRUE)
   require(purrr, quietly = TRUE)
+  require(Rfast, quietly = TRUE)
 
-  future_map(
-    seq_len(nrow(data)),
-    ~ {
-      dist_euclidean(
-        as.matrix(select(data, x, y, z))[.x, ],
-        projections[.x, -1]
-      )^2
-    },
-    .options = furrr_options(seed = TRUE)
-  ) %>%
-    reduce(c) %>%
-    mean()
+  data <- data |>
+    select(x, y, z) |>
+    as.matrix()
+  projections <- projections[, -1]
+
+  dist_vec <- rowsums((data - projections)^2, parallel = TRUE)
+
+  mean(dist_vec)
 }
