d5487af03292e960288626cebd87e1e5074c6c32
Robert Zielinski <rjz1996@outlook.com>
2026-04-13 15:16:24 -0400

    fix errors in output dimensions

--- code/functions/adni_modeling/final_projections.R
+++ code/functions/adni_modeling/final_projections.R
@@ -48,100 +48,108 @@ final_projections <- function(
 
   data.table::setDTthreads(1)
 
-  p <- progressor(length(id_values[unique(id_indices)]))
-  projection_list <- foreach(
-    id_idx = seq_along(id_values[unique(id_indices)]),
-    .options.future = list(seed = TRUE)
-  ) %dofuture%
-    {
-      group_idx <- unique(group_indices[id_indices == id_idx])
+  with_progress({
+    p <- progressor(length(id_values[unique(id_indices)]))
+    projection_list <- foreach(
+      id_idx = seq_along(id_values[unique(id_indices)]),
+      .options.future = list(seed = TRUE)
+    ) %dofuture%
+      {
+        group_idx <- unique(group_indices[id_indices == id_idx])
 
-      id_data <- surface_data[
-        id_indices == id_idx,
-        .(time_from_bl, x, y, z, partition, row_num)
-      ]
+        id_data <- surface_data[
+          id_indices == id_idx,
+          .(time_from_bl, x, y, z, partition, row_num)
+        ]
 
-      id_out <- list()
+        id_out <- list()
 
-      for (partition_val in partition_values) {
-        embedding_map <- additive_model[[partition_val]]$embeddings[[
-          id_idx
-        ]]$embedding_map
+        for (partition_val in partition_values) {
+          embedding_map <- additive_model[[partition_val]]$embeddings[[
+            id_idx
+          ]]$embedding_map
 
-        spline_coef_map <- additive_model[[partition_val]]$embeddings[[
-          id_idx
-        ]]$spline_coef_map
+          spline_coef_map <- additive_model[[partition_val]]$embeddings[[
+            id_idx
+          ]]$spline_coef_map
 
-        population_embedding <- additive_model[[
-          partition_val
-        ]]$population_embedding$embedding_map
-        group_embedding <- additive_model[[partition_val]]$group_embeddings[[
-          group_idx
-        ]]$embedding_map
+          population_embedding <- additive_model[[
+            partition_val
+          ]]$population_embedding$embedding_map
+          group_embedding <- additive_model[[partition_val]]$group_embeddings[[
+            group_idx
+          ]]$embedding_map
 
-        part_id_data <- id_data[partition == partition_val]
-        part_id_row_nums <- part_id_data$row_num
+          part_id_data <- id_data[partition == partition_val]
+          part_id_row_nums <- part_id_data$row_num
 
-        part_id_data_red <- part_id_data[, .(time_from_bl, x, y, z)] |>
-          as.matrix()
+          part_id_data_red <- part_id_data[, .(time_from_bl, x, y, z)] |>
+            as.matrix()
 
-        part_id_nearest_params <- nearest_params[part_id_row_nums, ]
+          part_id_nearest_params <- nearest_params[part_id_row_nums, ]
 
-        part_id_params <- map(
-          seq_len(nrow(part_id_data)),
-          ~ projection_additive_pme(
-            part_id_data_red[.x, ],
-            spline_coef_map,
-            part_id_nearest_params[.x, ],
-            param_grids[[partition_val]]
+          part_id_params <- map(
+            seq_len(nrow(part_id_data)),
+            ~ projection_additive_pme(
+              part_id_data_red[.x, ],
+              spline_coef_map,
+              part_id_nearest_params[.x, ],
+              param_grids[[partition_val]]
+            )
           )
-        )
 
-        part_id_params <- do.call(rbind, part_id_params)
+          part_id_params <- do.call(rbind, part_id_params)
 
-        part_id_projections <- apply(part_id_params, 1, embedding_map) |>
-          t()
+          part_id_projections <- apply(part_id_params, 1, embedding_map) |>
+            t()
 
-        part_id_pop_projections <- apply(
-          part_id_params,
-          1,
-          population_embedding
-        ) |>
-          t()
+          part_id_pop_projections <- apply(
+            part_id_params,
+            1,
+            population_embedding
+          ) |>
+            t()
 
-        part_id_group_projections <- apply(
-          part_id_params,
-          1,
-          group_embedding
-        ) |>
-          t()
+          part_id_group_projections <- apply(
+            part_id_params,
+            1,
+            group_embedding
+          ) |>
+            t()
 
-        part_id_group_projections <- cbind(
-          part_id_params[, 1],
-          part_id_pop_projections[, -1] + part_id_group_projections[, -1]
-        )
+          part_id_group_projections <- cbind(
+            part_id_params[, 1],
+            part_id_pop_projections[, -1] + part_id_group_projections[, -1]
+          )
 
-        id_out[[partition_val]] <- cbind(
-          part_id_row_nums,
-          part_id_params,
-          part_id_projections,
-          part_id_pop_projections,
-          part_id_group_projections
-        )
+          id_out[[partition_val]] <- cbind(
+            part_id_row_nums,
+            part_id_params,
+            part_id_projections,
+            part_id_pop_projections,
+            part_id_group_projections
+          )
+        }
+
+        p(sprintf("ID %d of %d", id_idx, length(id_values)))
+
+        id_out <- do.call(rbind, id_out)
       }
-
-      p(sprintf("ID %d of %d", id_idx, length(id_values)))
-
-      id_out <- do.call(rbind, id_out)
-    }
+  })
 
   projection_mat <- do.call(rbind, projection_list)
   projection_mat <- projection_mat[order(projection_mat[, 1]), -1]
 
   param_mat <- projection_mat[, 1:(d + 1)]
-  projections <- projection_mat[, (d + 2):(d + 1 + D)]
-  pop_projections <- projection_mat[, (d + 2 + D):(d + 1 + 2 * D)]
-  group_projections <- projection_mat[, (d + 2 + 2 * D):(d + 1 + 3 * D)]
+  projections <- projection_mat[, (d + 2):((d + 1) + (D + 1))]
+
+  pop_projections <- projection_mat[,
+    ((d + 1) + (D + 1) + 1):((d + 1) + 2 * (D + 1))
+  ]
+
+  group_projections <- projection_mat[,
+    ((d + 1) + 2 * (D + 1) + 1):((d + 1) + 3 * (D + 1))
+  ]
 
   list(
     params = param_mat,
