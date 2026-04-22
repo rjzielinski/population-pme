17b2269ed30e439691ffb5e090a4d58a6831347c
Robert Zielinski <rjz1996@outlook.com>
2026-04-22 12:04:57 -0400

    allow different cluster summaries

--- code/functions/adni_modeling/data_reduction.R
+++ code/functions/adni_modeling/data_reduction.R
@@ -2,7 +2,10 @@ data_reduction <- function(
   lhipp_surface,
   lhipp_groups,
   lhipp_ids,
-  lhipp_scans
+  lhipp_scans,
+  min_clusters = 50,
+  component_type = "centers",
+  subsample_size = 5
 ) {
   require(doFuture, quietly = TRUE)
   require(dplyr, quietly = TRUE)
@@ -33,6 +36,7 @@ data_reduction <- function(
     .options.future = list(seed = TRUE)
   ) %dofuture%
     {
+      # print(scan_idx)
       scan_indices <- lhipp_scans == lhipp_scans[scan_idx]
 
       group_val <- lhipp_surface |>
@@ -59,14 +63,22 @@ data_reduction <- function(
       scan_time <- unique(scan_df$time_from_bl)
 
       for (partition_idx in seq_along(partition_values)) {
+        # print(partition_idx)
+
         scan_data[[partition_idx]] <- scan_df |>
           filter(partition == partition_values[partition_idx]) |>
           select(x, y, z) |>
           as.matrix()
 
+        partition_min_clusters <- ifelse(
+          nrow(scan_data[[partition_idx]]) <= min_clusters,
+          nrow(scan_data[[partition_idx]]) / 2,
+          min_clusters
+        )
+
         scan_red[[partition_idx]] <- hdmde(
           scan_data[[partition_idx]],
-          50,
+          partition_min_clusters,
           0.05,
           200
         )
@@ -75,16 +87,63 @@ data_reduction <- function(
         scan_weights[[partition_idx]] <- scan_red[[partition_idx]]$theta_hat
         scan_n[[partition_idx]] <- nrow(scan_centers[[partition_idx]])
 
-        scan_red[[partition_idx]] <- cbind(
-          scan_centers[[partition_idx]],
-          scan_weights[[partition_idx]],
-          rep(group_val, scan_n[[partition_idx]]),
-          rep(id_val, scan_n[[partition_idx]]),
-          rep(lhipp_scans[scan_idx], scan_n[[partition_idx]]),
-          rep(scan_time, scan_n[[partition_idx]]),
-          rep(partition_values[partition_idx], scan_n[[partition_idx]])
-        ) |>
-          as_tibble(.name_repair = c("minimal"))
+        if (component_type == "centers") {
+          scan_red[[partition_idx]] <- cbind(
+            scan_centers[[partition_idx]],
+            scan_weights[[partition_idx]],
+            rep(group_val, scan_n[[partition_idx]]),
+            rep(id_val, scan_n[[partition_idx]]),
+            rep(lhipp_scans[scan_idx], scan_n[[partition_idx]]),
+            rep(scan_time, scan_n[[partition_idx]]),
+            rep(partition_values[partition_idx], scan_n[[partition_idx]])
+          ) |>
+            as_tibble(.name_repair = c("minimal"))
+        } else if (component_type == "subsample") {
+          partition_km <- scan_red[[partition_idx]]$km
+
+          cluster_points <- foreach(
+            cluster_val = seq_len(scan_n[[partition_idx]])
+          ) %do%
+            {
+              n_obs <- partition_km$size[cluster_val]
+
+              cluster_obs <- scan_data[[partition_idx]][
+                partition_km$cluster == cluster_val,
+                ,
+                drop = FALSE
+              ]
+
+              n_points <- min(n_obs, subsample_size)
+
+              if (n_obs >= subsample_size) {
+                sampled_indices <- sample(n_obs, subsample_size)
+                cluster_points <- cluster_obs[sampled_indices, ]
+                point_weights <- rep(
+                  scan_weights[[partition_idx]][cluster_val] / subsample_size,
+                  subsample_size
+                )
+              } else {
+                cluster_points <- cluster_obs
+                point_weights <- rep(
+                  scan_weights[[partition_idx]][cluster_val] / n_obs,
+                  n_obs
+                )
+              }
+
+              cbind(
+                cluster_points,
+                point_weights,
+                rep(group_val, n_points),
+                rep(id_val, n_points),
+                rep(lhipp_scans[scan_idx], n_points),
+                rep(scan_time, n_points),
+                rep(partition_values[partition_idx], n_points)
+              )
+            }
+          scan_red[[partition_idx]] <- do.call(rbind, cluster_points) |>
+            as_tibble(.name_repair = c("minimal"))
+        }
+
         names(scan_red[[partition_idx]]) <- lhipp_red_names
         scan_red[[partition_idx]] <- scan_red[[partition_idx]] |>
           mutate_at(c("x", "y", "z", "weight", "time_from_bl"), as.numeric)
