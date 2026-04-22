ad3f564b1b93894c3ba649c1d4821f9b4fb6ffe3
Robert Zielinski <rjz1996@outlook.com>
2026-04-22 12:05:51 -0400

    save reconstructions

--- code/functions/adni_modeling/get_init_params.R
+++ code/functions/adni_modeling/get_init_params.R
@@ -1,12 +1,23 @@
 get_init_params <- function(init_lpme, centers) {
   init_params <- list()
+  init_reconstructions <- list()
 
   for (partition_idx in seq_len(length(init_lpme))) {
-    init_params[[partition_idx]] <- calculate_lpme_reconstructions(
+    partition_reconstructions <- calculate_lpme_reconstructions(
       init_lpme[[partition_idx]],
       centers[[partition_idx]]
-    )$projections
+    )
+
+    init_params[[partition_idx]] <- partition_reconstructions$projections
+    init_reconstructions[[
+      partition_idx
+    ]] <- partition_reconstructions$reconstructions
   }
 
-  init_params
+  init_reconstructions <- do.call(rbind, init_reconstructions)
+
+  list(
+    params = init_params,
+    reconstructions = init_reconstructions
+  )
 }
