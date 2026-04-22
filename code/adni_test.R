e76fc3ff9318447596c4d00a9bfd6db2f93b0f74
Robert Zielinski <rjz1996@outlook.com>
2026-04-22 12:07:37 -0400

    use function for additive pme fitting

--- code/adni_test.R
+++ code/adni_test.R
@@ -20,6 +20,7 @@ cores <- detectCores() - 4
 
 plot_progress <- TRUE
 
+source("code/functions/additive_pme.R")
 source("code/functions/fit_weighted_spline.R")
 source("code/functions/print_SSD.R")
 source("code/functions/fit_adni_additive_model.R")
@@ -41,7 +42,7 @@ verbose <- TRUE
 epsilon <- 0.05
 max_iter <- 100
 
-read_data(n_individuals = 100)
+read_data(n_individuals = 200, n_partitions = 4, y_bound = 0.2)
 
 # INITIALIZATION
 
@@ -49,7 +50,8 @@ lhipp_init_lpme <- lpme_initialization(
   data = lhipp_surface,
   ids = lhipp_ids,
   d = 2,
-  min_clusters = 50,
+  init_type = "centers",
+  min_clusters = 20,
   print_plots = FALSE,
   verbose = FALSE
 )
@@ -64,7 +66,9 @@ lhipp_surface_red <- data_reduction(
   lhipp_surface,
   lhipp_groups,
   lhipp_ids,
-  lhipp_scans
+  lhipp_scans,
+  min_clusters = 20,
+  component_type = "centers"
 )
 
 prepare_data(lhipp_surface, lhipp_surface_red)
@@ -72,187 +76,43 @@ prepare_data(lhipp_surface, lhipp_surface_red)
 # PARAMETERIZATION
 
 init_params <- get_init_params(lhipp_init_lpme, lhipp_centers)
+init_embeddings <- init_params$reconstructions
+init_params <- init_params$params
 
 partition_values <- unique(lhipp_partition)
 partition_values <- partition_values[order(partition_values)]
 
-if (plot_progress == TRUE) {
-  init_embeddings <- list()
-  for (partition_idx in seq_along(partition_values)) {
-    init_embeddings[[partition_idx]] <- future_map(
-      seq_len(nrow(init_params[[partition_idx]])),
-      ~ lhipp_init_lpme[[partition_idx]]$embedding_map(unlist(init_params[[
-        partition_idx
-      ]][.x, ])),
-      .options = furrr_options(seed = TRUE)
-    ) |>
-      reduce(rbind)
-  }
-  init_embeddings_full <- reduce(init_embeddings, rbind)
+group_vals <- sort(unique(lhipp_surface_red$group))
 
+if (plot_progress == TRUE) {
   plot_ly(
-    x = init_embeddings_full[, 2],
-    y = init_embeddings_full[, 3],
-    z = init_embeddings_full[, 4],
-    frame = init_embeddings_full[, 1],
+    x = init_embeddings[, 2],
+    y = init_embeddings[, 3],
+    z = init_embeddings[, 4],
+    frame = init_embeddings[, 1],
     type = "scatter3d",
     mode = "markers",
     marker = list(size = 3)
   )
 }
 
-group_vals <- unique(lhipp_surface_red$group)[order(unique(
-  lhipp_surface_red$group
-))]
-
-# Fit initial additive model estimate
-additive_model <- list()
-for (partition_idx in seq_along(partition_values)) {
-  additive_model[[partition_idx]] <- fit_adni_additive_model(
-    centers = lhipp_centers[[partition_idx]],
-    params = init_params[[partition_idx]],
-    weights = lhipp_weights[[partition_idx]],
-    lambda = exp(-20:5),
-    gamma = exp(-20:5),
-    groups = filter(
-      lhipp_surface_red,
-      partition == partition_values[partition_idx]
-    )$group,
-    ids = filter(
-      lhipp_surface_red,
-      partition == partition_values[partition_idx]
-    )$id,
-    scans = filter(
-      lhipp_surface_red,
-      partition == partition_values[partition_idx]
-    )$scan,
-    times = filter(
-      lhipp_surface_red,
-      partition == partition_values[partition_idx]
-    )$time_from_bl,
-    epsilon = 0.01,
-    max_iter = 250,
-    cores = cores
-  )
-}
-
-if (plot_progress == TRUE) {
-  plot_additive_model(
-    additive_model,
-    init_params,
-    group_vals,
-    margin = 0.02
-  )
-}
-
-print("Updating parameters")
-
-param_list <- update_params(
-  additive_model,
-  init_params,
-  lhipp_surface_red,
-  lhipp_centers,
-  partition_values,
-  group_values,
-  id_values
+additive_model_list <- additive_pme(
+  reduced_data = lhipp_surface_red,
+  centers = lhipp_centers,
+  init_params = init_params,
+  weights = lhipp_weights,
+  groups = lhipp_surface_red$group,
+  ids = lhipp_surface_red$id,
+  scans = lhipp_surface_red$scan,
+  times = lhipp_surface_red$time_from_bl,
+  partitions = lhipp_surface_red$partition,
+  cores = cores,
+  plot_progress = TRUE,
 )
 
-params <- param_list$params
-center_projections <- param_list$center_projections
-
-ssd <- calc_ssd(lhipp_centers, center_projections, partition_values)
-ssd_ratio <- 10 * epsilon
-
-n <- 1
-
-ssd_vec <- vector()
-ssd_vec[n] <- ssd
-
-
-while (
-  (ssd_ratio > epsilon) &
-    (ssd_ratio <= ssd_ratio_threshold) &
-    (n <= (max_iter - 1))
-) {
-  ssd_prev <- ssd
-  additive_model_old <- additive_model
-  params_prev <- params
-
-  print("Fitting additive models")
-
-  for (partition_idx in seq_along(partition_values)) {
-    additive_model[[partition_idx]] <- fit_adni_additive_model(
-      centers = lhipp_centers[[partition_idx]],
-      params = init_params[[partition_idx]],
-      weights = lhipp_weights[[partition_idx]],
-      lambda = exp(-20:5),
-      gamma = exp(-20:5),
-      groups = filter(
-        lhipp_surface_red,
-        partition == partition_values[partition_idx]
-      )$group,
-      ids = filter(
-        lhipp_surface_red,
-        partition == partition_values[partition_idx]
-      )$id,
-      scans = filter(
-        lhipp_surface_red,
-        partition == partition_values[partition_idx]
-      )$scan,
-      times = filter(
-        lhipp_surface_red,
-        partition == partition_values[partition_idx]
-      )$time_from_bl,
-      epsilon = 0.01,
-      max_iter = 250,
-      cores = cores
-    )
-  }
-
-  if (plot_progress == TRUE) {
-    fig <- plot_additive_model(
-      additive_model,
-      params,
-      group_vals,
-      margin = 0.02
-    )
-    print(fig)
-  }
-
-  print("Updating parameters")
-
-  param_list <- update_params(
-    additive_model,
-    params,
-    lhipp_surface_red,
-    lhipp_centers,
-    partition_values,
-    group_values,
-    id_values
-  )
-
-  params <- param_list$params
-  center_projections <- param_list$center_projections
-
-  ssd <- calc_ssd(lhipp_centers, center_projections, partition_values)
-  ssd_ratio <- abs(ssd - ssd_prev) / ssd_prev
-
-  n <- n + 1
-  ssd_vec[n] <- ssd
-
-  if (verbose == TRUE) {
-    print_ssd(ssd, ssd_ratio, n)
-  }
-}
-
-if (plot_progress == TRUE) {
-  plot_additive_model(
-    additive_model,
-    params,
-    group_vals,
-    margin = 0.02
-  )
-}
+additive_model <- additive_model_list$additive_model
+params <- additive_model_list$params
+center_projections <- additive_model_list$center_projections
 
 # calculate final MSD and projections?
 print("Computing full projections")
@@ -285,4 +145,4 @@ lhipp_test_out <- list(
   partition_values = partition_values,
   msd = msd
 )
-saveRDS(lhipp_test_out, "output/lhipp_additive_model_100.RDS")
+saveRDS(lhipp_test_out, "output/lhipp_additive_model_200.RDS")
